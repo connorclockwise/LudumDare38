4f6eda26088fc0015f9a50b56f853969b0367843
TestSubject06 <zzzaaccckk@yahoo.com>
2017-04-21 22:30:47 -0400

    MERRY CHRISTMAS PILGRIMS

---
+++ source/FlxStarfield.hx
@@ -0,0 +1,65 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import openfl.geom.Rectangle;
+
+/**
+ * ...
+ * @author Zack
+ */
+
+typedef Star = {
+	var x:Float;
+	var y:Float;
+	var vScale:Float;
+}
+ 
+class FlxStarfield extends FlxSprite
+{
+	private var stars:Array<Star>;
+	public var speed = 100;
+	public function new(X:Float, Y:Float, Width:Float, Height:Float) 
+	{
+		super(X, Y);
+		stars = new Array<Star>();
+		for (i in 0...300) {
+			stars.push( {
+				x: FlxG.random.int(0, Std.int(Width)),
+				y: FlxG.random.int(0, Std.int(Height)),
+				vScale: FlxG.random.float(1, 3)
+			});
+		}
+		makeGraphic(Std.int(Width), Std.int(Height), 0x0, true);
+	}
+	
+	override public function update(elapsed:Float) {
+		FlxG.log.add(stars[0].y);
+		for (star in stars) {
+			star.y += speed * star.vScale * elapsed;
+			if (star.y > 640) {
+				star.y -= 640;
+			}
+		}
+		speed = FlxG.mouse.x;
+		super.update(elapsed);
+	}
+	
+	override public function draw():Void 
+	{
+		pixels.lock();
+		pixels.fillRect(new Rectangle(0, 0, width, height), 0x0);
+		for (star in stars) {
+			pixels.setPixel32(Std.int(star.x), Std.int(star.y), 0xFFFFFFFF);
+		}
+		dirty = true;
+		pixels.unlock();
+		super.draw();
+	}
+	
+	
+	
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,18 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	
+	public var starfield:FlxStarfield;
+	
 	override public function create():Void
 	{
 		super.create();
+		starfield = new FlxStarfield(0, 0, FlxG.width, FlxG.height);
+		add(starfield);
 	}
 
 	override public function update(elapsed:Float):Void
