29ce585dac399428eab2aceb435c35e6853d2673
connorclockwise <connorclockwise@gmail.com>
2017-04-22 10:43:18 -0400

    Another pass at reticule logic.

--- source/SlingShotHud.hx
+++ source/SlingShotHud.hx
@@ -6,6 +6,7 @@ import flixel.group.FlxGroup;
 import flixel.math.FlxAngle;
 import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 
 class SlingShotHud extends FlxTypedGroup<FlxSprite>
 {
@@ -17,6 +18,8 @@ class SlingShotHud extends FlxTypedGroup<FlxSprite>
 	public var _slingshotStart:FlxPoint;
 	public var _slingshotEnd:FlxPoint;
 	public var _slingshotPointer:FlxSprite;
+	public var _canReleaseSlingshot:Bool;
+	public var _slingshotTimer:FlxTimer;
 
 	public var _player:FlxSprite;
 
@@ -36,43 +39,86 @@ class SlingShotHud extends FlxTypedGroup<FlxSprite>
 		_player = player;
 
 		_slingshotPointer = new FlxSprite(0,0);
-		_reticuleTrail2.makeGraphic(4, 4, FlxColor.WHITE);
+		_slingshotPointer.makeGraphic(20, 50, FlxColor.WHITE);
+		_slingshotPointer.origin.set(10, 0);
+		_slingshotPointer.kill();
+
+		_slingshotTimer = new FlxTimer();
 
 		add(_reticule);
 		add(_reticuleTrail1);
 		add(_reticuleTrail2);
+		add(_slingshotPointer);
 	}
 
 	public function positionAroundOrigin(){
-		var mousePosition:FlxPoint = FlxG.mouse.getScreenPosition();
-		var toMouse:FlxPoint = mousePosition.subtractPoint(_player.getScreenPosition());
+		var mousePosition:FlxPoint = FlxG.mouse.getWorldPosition();
+		var toMouse:FlxPoint = mousePosition.subtractPoint(_player.getMidpoint());
 		toMouse = FlxAngle.getPolarCoords(toMouse.x, toMouse.y);
 		var offset:Float = Math.min(Math.max(toMouse.x, _minOffset), _maxOffset);
 		var resultantPosition:FlxPoint = FlxAngle.getCartesianCoords(offset, toMouse.y);
-
-		_reticule.x = resultantPosition.x + _player.x;
-		_reticule.y = resultantPosition.y + _player.y;
+		var newPosition:FlxPoint = resultantPosition.addPoint(_player.getMidpoint());
+		_reticule.setPosition(
+			newPosition.x - _reticule.width/2,
+			newPosition.y - _reticule.height/2
+		);
 
 		resultantPosition = FlxAngle.getCartesianCoords(offset * 2 / 3, toMouse.y);
-
-		_reticuleTrail1.x = resultantPosition.x + _player.x;
-		_reticuleTrail1.y = resultantPosition.y + _player.y;
+		newPosition = resultantPosition.addPoint(_player.getMidpoint());
+		_reticuleTrail1.setPosition(
+			newPosition.x - _reticuleTrail1.width/2,
+			newPosition.y - _reticuleTrail1.height/2
+		);
 
 		resultantPosition = FlxAngle.getCartesianCoords(offset * 1 / 3, toMouse.y);
-
-		_reticuleTrail2.x = resultantPosition.x + _player.x;
-		_reticuleTrail2.y = resultantPosition.y + _player.y;
+		newPosition = resultantPosition.addPoint(_player.getMidpoint());
+		_reticuleTrail2.setPosition(
+			newPosition.x - _reticuleTrail2.width/2,
+			newPosition.y - _reticuleTrail2.height/2
+		);
 
 	}
 
 
 	public function handleClick():Void{
-		//FlxG.
+		if (FlxG.mouse.justPressed)
+	    {
+	    	_slingshotStart = FlxG.mouse.getWorldPosition();
+	    	_reticule.kill();
+	    	_reticuleTrail1.kill();
+	    	_reticuleTrail2.kill();
+	    	_slingshotPointer.reset(0,0);
+	    	_slingshotPointer.width = 20;
+	        _slingshotTimer.start(0.1, allowReleaseSlingshot, 0);
+
+	    	_slingshotStart = FlxG.mouse.getWorldPosition();
+	    	_slingshotPointer.setPosition(_slingshotStart.x, _slingshotStart.y);
+	    	var slingShotPointAngle:Float = FlxAngle.angleBetweenPoint( _player, _slingshotStart, true);
+	    	slingShotPointAngle -= 90;
+	    	_slingshotPointer.angle = slingShotPointAngle;
+	    }
+
+	    if (FlxG.mouse.pressed)
+	    {
+	    	_slingshotEnd = FlxG.mouse.getWorldPosition();
+			var toSlingShotEnd:FlxPoint = _slingshotStart.subtractPoint(_slingshotEnd);
+			toSlingShotEnd = FlxAngle.getPolarCoords(toSlingShotEnd.x, toSlingShotEnd.y);
+	    	_slingshotPointer.scale.set(1, toSlingShotEnd.y/50);
+	    }
+
+	    if (_canReleaseSlingshot && FlxG.mouse.justReleased)
+	    {
+	        // The left mouse button has just been released
+	    }
+	}
+
+	public function allowReleaseSlingshot(_){
+		 _canReleaseSlingshot = true;
 	}
 
 	override public function update (elapsed:Float){
-		positionAroundOrigin();
 		super.update(elapsed);
+		positionAroundOrigin();
 		handleClick();
 	}
 }
